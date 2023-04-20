f81b36fa3ca1ef399bc50e6a7fd9cb4c455636d5
Dennis KIbet <denniskibet36@gmail.com.com>
2023-04-20 17:46:42 +0300

    Navbar Edited

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,52 @@
 import React from "react";
 
 const Navbar = () => {
-    return (
-     <header className="flex text-dark justify-between items-center h-20 max-w-[1240px] px-4 mx-auto">
+  return (
+    <header className="text-dark px-4 mx-auto">
+      <div className="flex justify-between">
         <div className="">
-            <div className="w-full bg-dark flex max-w-lg">
-                <h1 className="text-[14px] w-full font-bold text-blue ">
-                    <a href="/" title="Car Hire"> Car Hire | Lets Go</a>
-                </h1>
-            </div>
-            <div className="w-10 ">
-                <nav className="">
-                    <ul className="flex">
-                        <li className="p-4">
-                            <a href="/">Home</a>
-                        </li>
-                        <li className="p-4">
-                            <a href="/">Listings</a>
-                        </li>
-                        <li className="p-4">
-                            <a href="/">Contact</a>
-                        </li>
-                        <li className="p-4">
-                            <a href="/">Services</a>
-                        </li>
-                        <li className="p-4">
-                            <a href="/">About</a>
-                        </li>
-                        <li className="p-4">
-                            <a href="/">Login</a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
+          <h1 className="text-[14px] font-bold ">
+            <a href="/" title="Car Hire">
+              {" "}
+              Car Hire | Lets Go
+            </a>
+          </h1>
         </div>
-     </header>
-    )
-}
+        <div>
+          {/* Shopping cart icon */}
+          <svg
+            className="w-6 h-6 cursor-pointer"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            aria-label="Shopping Cart"
+          >
+            {/* Icon path goes here */}
+          </svg>
+        </div>
+        <ul className="flex items-center">
+        <li className="p-4 hover:text-blue hover:bg-gray-200">
+            <a href="/">Home</a>
+          </li>
+          <li className="p-4 hover:text-blue hover:bg-gray-200">
+            <a href="/">Listings</a>
+          </li>
+          <li className="p-4 hover:text-blue hover:bg-gray-200">
+            <a href="/">Contact</a>
+          </li>
+          <li className="p-4 hover:text-blue hover:bg-gray-200">
+            <a href="/">Services</a>
+          </li>
+          <li className="p-4 hover:text-blue hover:bg-gray-200">
+            <a href="/">About</a>
+          </li>
+          <li className="p-4 hover:text-blue hover:bg-gray-200">
+            <a href="/">Login</a>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
